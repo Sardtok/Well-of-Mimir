1666e74f01bce832d66f26201611571ce0292167
Sigmund Hansen <sigmunha@student.uio.no>
2011-10-19 20:25:54 +0200

    course.py - Initial model of a course

---
+++ core/models/course.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
+# Represents courses that students can ask questions about
+class Course(models.Model):
+    class Meta:
+        app_label = "mimir"
+        verbose_name = _("Course")
+        verbose_plural = _("Courses")
+    
+    # Length was arbitrarily chosen - may be extended in the future
+    name = CharField(max_length=50)
