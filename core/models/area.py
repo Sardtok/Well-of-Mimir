3884cf71f14e34a3951f1f2288a48f6ea93331d8
Sigmund Hansen <sigmunha@student.uio.no>
2011-10-19 20:26:54 +0200

    area.py - Initial model of an area/map

---
+++ core/models/area.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils.translation import ugettext as _
+
+# Represents maps for an area.
+# An area can be something like a building, floor or room.
+class Area(models.Model):
+    class Meta:
+        app_label = "mimir"
+        verbose_name = _("Area")
+        verbose_plural = _("Areas")
+
+    short_name = SlugField()
+    full_name = CharField()
+    image_type = SlugField()
+
+    parent = ForeignKeyField(Map, related_name="areas", blank=True)
+    height = DecimalField(blank=True)
+    width = DecimalField(blank=True)
+    x = DecimalField(blank=True)
+    y = DecimalField(blank=True)
