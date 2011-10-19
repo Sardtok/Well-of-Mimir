d1672ec31dde8c88394e5925f35345c5b72fcef7
Sigmund Hansen <sigmunha@student.uio.no>
2011-10-19 20:26:21 +0200

    oracle.py - Initial model of an oracle/TA

---
+++ core/models/oracle.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext as _
+
+from mimir.core.models.course import Course
+
+# Data about an oracle/TA in the system
+class Oracle (models.Model):
+    class Meta:
+        app_label = "mimir"
+        verbose_name = _("Oracle")
+        verbose_plural = _("Oracles")
+
+    user = ForeignKeyField(User, related_name="oracles", verbose_name=_("User"))
+    courses = ManyToManyField(Course, related_name="oracles", verbose_name=_("Courses"), blank=True)
